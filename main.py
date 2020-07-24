924679cb5e2b46f71141bf6a4f5de22a8f8dd95c
Pavleg <pajabonki@gmail.com>
2020-07-24 18:17:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+name = input("hello what is your name? \n")
+print(f"hello {name}! lets start.")
+
+print()
+
+holiday = input("enter a name of a holiday: ")
+place = input("enter a place: ")
+#hero = input("enter a name of a hero:")
+outfit = input("what would you like to wear: ")
+#power = input("what superpowers would you like to have: ")
+animal = input("name an animal: ")
+celebrity = input("name a celebrity: ")
+object1 = input("name an object: ")
+celebrity2 = input("name a different celebrity: ")
+proffesion = input("name a proffession: ")
+
+
+print(f"Last {holiday} i was taking a trip to {place} and while walking I spotted {celebrity}. I then saw that he was holding a {animal}. I was shocked to see him with a {animal} I almost didn't even notice that {celebrity2} was trying to juggle an {object1} while wearing a {outfit}. I later found out that {celebrity} was holding that animal to prepare for his job as a {proffesion} and {celebrity2} was trying to join the circus.")
